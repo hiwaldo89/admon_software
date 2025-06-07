4f42cf0057da0f2f3911fb4c8e4b3802360730e2
Oswaldo Zarate <oswaldozarate@Arelis-MBP-2.attlocal.net>
2025-06-06 19:39:27 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+import joblib
+import numpy as np
+import pandas as pd
+
+app = FastAPI()
+model = joblib.load("model.pkl")
+
+@app.post("/predict")
+async def predict(request: Request):
+    data = await request.json()
+    df = pd.DataFrame([data])
+    pred = model.predict(df)
+    return {"prediction": float(pred[0])}
